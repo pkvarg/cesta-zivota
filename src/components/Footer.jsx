56e627ef055eec0fb61bd893c9fe07dc3708c269
peter varga <pkvarg@yahoo.se>
2023-09-06 10:18:12 +0200

    corr 06sep

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,26 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-center !text-[#47464a] text-[20px] pt-[95px] pb-[25px]'>
+    <div className='text-center  text-[20px] pt-[95px] pb-[25px]'>
+      <div className='flex flex-col lg:flex-row justify-center gap-0 lg:gap-2 pt-4 mb-8'>
+        <a href='https://hladamboha.sk' target='_blank'>
+          hladamboha.sk
+        </a>{' '}
+        <span className='hidden lg:block'>|</span>
+        <a href='https://fireproduction.sk' target='_blank'>
+          fireproduction.sk
+        </a>{' '}
+        <span className='hidden lg:block'>|</span>
+        <a href='https://radio7.sk' target='_blank'>
+          Rádio 7
+        </a>
+      </div>
+
       <a
         href='https://www.pictusweb.sk'
         target='_blank'
         rel='noreferrer'
-        className='text-center'
+        className='text-center !text-[#47464a]'
       >
         &#60;&#47;&#62; PICTUSWEB Development
       </a>
--- src/sections/Section05.jsx
+++ src/sections/Section05.jsx
@@ -13,26 +13,20 @@ const Section05 = () => {
           </a>
           , aby si bol plný Jeho Slova a podľa Neho žil. Ideálne ako prvé
           Evanjelium Jána a pravidelne navštevuj nejaké spoločenstvo, kde
-          oslavujú Boha Otca, Ježiša a Svätého Ducha; nie Máriu ani Jehovu ani
-          nikoho iného.
-        </p>
-        <div className='flex flex-col lg:flex-row gap-2 pt-4'>
-          <p> Ak chceš viac, skús napríklad:</p>
-          <a className='underline' href='https://hladamboha.sk' target='_blank'>
-            hladamboha.sk,
+          oslavujú Boha Otca, Ježiša a Svätého Ducha; nie Jehovu, viac na{' '}
+          <a href='https://svedkovia.sk' target='_blank' className='underline'>
+            svedkovia.sk
           </a>
+          . K Márii, matke Ježišovej, pozri{' '}
           <a
-            className='underline'
-            href='https://fireproduction.sk'
+            href='https://katolickaviera.sk'
             target='_blank'
+            className='underline'
           >
-            fireproduction.sk,
+            katolickaviera.sk
           </a>
-          <p>alebo</p>
-          <a className='underline' href='https://radio7.sk' target='_blank'>
-            Rádio 7.
-          </a>
-        </div>
+          .
+        </p>
         <p className='pt-4'>
           Prajem ti, aby si uveril pravde, ktorou je Božie Slovo a získal tak
           späť postavenie, ktoré ti patrí.
