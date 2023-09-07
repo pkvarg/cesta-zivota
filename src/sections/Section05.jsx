200c9a90c45f68d26c5ba19b5f5d81b42f3da28f
peter varga <pkvarg@yahoo.se>
2023-09-07 11:07:44 +0200

    links corr

--- src/sections/Section05.jsx
+++ src/sections/Section05.jsx
@@ -13,21 +13,33 @@ const Section05 = () => {
           </a>
           , aby si bol plný Jeho Slova a podľa Neho žil. Ideálne ako prvé
           Evanjelium Jána a pravidelne navštevuj nejaké spoločenstvo, kde
-          oslavujú Boha Otca, Ježiša a Svätého Ducha; nie Jehovu, viac na{' '}
-          <a href='https://svedkovia.sk' target='_blank' className='underline'>
-            svedkovia.sk
-          </a>
-          . K Márii, matke Ježišovej, pozri{' '}
-          <a
-            href='https://katolickaviera.sk'
-            target='_blank'
-            className='underline'
-          >
-            katolickaviera.sk
-          </a>
-          .
+          oslavujú Boha Otca, Ježiša a Svätého Ducha.
+          <p className='text-center mt-8'>
+            {' '}
+            Sú všetky cirkevné tradície podložené Božím Slovom? Viac na{' '}
+            <a
+              href='https://katolickaviera.sk'
+              target='_blank'
+              className='underline'
+            >
+              katolickaviera.sk
+            </a>
+            .
+          </p>
+          <p className='text-center mt-4'>
+            {' '}
+            Hlásajú svedkovia Jehovovi evanjelium, aké hlásali apoštoli? Viac na{' '}
+            <a
+              href='https://svedkovia.sk'
+              target='_blank'
+              className='underline'
+            >
+              svedkovia.sk
+            </a>
+            .{' '}
+          </p>
         </p>
-        <p className='pt-4'>
+        <p className='text-center mt-4'>
           Prajem ti, aby si uveril pravde, ktorou je Božie Slovo a získal tak
           späť postavenie, ktoré ti patrí.
         </p>
